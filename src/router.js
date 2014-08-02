75795ff5b30f906a0e121f1aba668961d0f93948
Ethan Zimmerman <thebinarypenguin@gmail.com>
2014-08-02 10:48:50 -0400

    Move parse functions from router.js to utils.js
    * parseDelay
    * parseCustomStatusCode
    * parseCustomHeaders

--- src/router.js
+++ src/router.js
@@ -4,73 +4,6 @@ var _       = require('underscore'),
 
 function SiteRouter() {
 
-  var parseDelay = function(delay) {
-    var lowered = delay.toLowerCase();
-    var hCount, mCount, sCount = 0;
-    var pieces = null;
-    var milliseconds = 0;
-
-    // Test for disallowed characters
-    if (/^[0-9hms]+$/.test(lowered) === false) {
-      return null;
-    }
-
-    // Count occurrences of the letters 'h', 'm', and 's'
-    for (var a=0; a<lowered.length; a++) {
-      if (lowered[a] === 'h') { hCount++; }
-      if (lowered[a] === 'm') { mCount++; }
-      if (lowered[a] === 's') { sCount++; }
-    }
-
-    // Test for too many occurrences of the letters 'h', 'm', and 's'
-    if (hCount > 1 || mCount > 1 || sCount > 1) {
-      return null;
-    }
-
-    // Parse delay into pieces
-    pieces = /^([0-9]+[h|m|s])?([0-9]+[h|m|s])?([0-9]+[h|m|s])?$/.exec(lowered);
-    if (pieces === null) {
-      return null;
-    }
-
-    // Calculate milliseconds from pieces
-    for (var b=1; b<=3; b++) {
-      if (pieces[b]) {
-        if (pieces[b].slice(-1) === 'h') { milliseconds += pieces[b].slice(0, -1) * 1000 * 60 * 60; }
-        if (pieces[b].slice(-1) === 'm') { milliseconds += pieces[b].slice(0, -1) * 1000 * 60; }
-        if (pieces[b].slice(-1) === 's') { milliseconds += pieces[b].slice(0, -1) * 1000; }
-      }
-    }
-
-    return milliseconds;
-  };
-
-  var parseCustomStatusCode = function(status) {
-    if (_.isUndefined(status) || _.isNull(status) || _.isArray(status) || _.isObject(status)) {
-      return null;
-    }
-
-    return status;
-  };
-
-  var parseCustomHeaders = function(headers) {
-    if (!_.isObject(headers)) {
-      return null;
-    }
-
-    var err = _.find(_.values(headers), function(v) {
-      if (_.isUndefined(v) || _.isNull(v) || _.isArray(v) || _.isObject(v)) {
-        return true;
-      }
-    })
-
-    if (err) {
-      return null;
-    }
-
-    return headers;
-  };
-
   var cors = function(req, res, next) {
     if (req.method === 'OPTIONS') {
 
@@ -93,7 +26,7 @@ function SiteRouter() {
   };
 
   var delay = function(req, res, next) {
-    var timeout = parseDelay(req.params.delay);
+    var timeout = utils.parseDelay(req.params.delay);
 
     if (timeout === null) {
       return res.send(400, utils.formatJSON({ message: 'Invalid Delay' }));
@@ -108,7 +41,7 @@ function SiteRouter() {
 
     // Custom Status Code
     if (_.has(payload, "_status")) {
-      var customStatusCode = parseCustomStatusCode(payload._status);
+      var customStatusCode = utils.parseCustomStatusCode(payload._status);
 
       if (customStatusCode === null) {
         return res.send(400, utils.formatJSON({ message: 'Invalid Status Code' }));
@@ -119,7 +52,7 @@ function SiteRouter() {
 
     // Custom Headers
     if (_.has(payload, "_headers")) {
-      var customHeaders = parseCustomHeaders(payload._headers);
+      var customHeaders = utils.parseCustomHeaders(payload._headers);
 
       if (customHeaders === null) {
         return res.send(400, utils.formatJSON({ message: 'Invalid Headers' }));
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 exports.errorHandler = function(err, req, res, next) {
 
   // If the bodyParser middleware chokes on bad JSON it
@@ -12,4 +14,71 @@ exports.errorHandler = function(err, req, res, next) {
 
 exports.formatJSON = function(obj) {
   return JSON.stringify(obj, null, '  ') + '\n';
+};
+
+exports.parseDelay = function(delay) {
+  var lowered = delay.toLowerCase();
+  var hCount, mCount, sCount = 0;
+  var pieces = null;
+  var milliseconds = 0;
+
+  // Test for disallowed characters
+  if (/^[0-9hms]+$/.test(lowered) === false) {
+    return null;
+  }
+
+  // Count occurrences of the letters 'h', 'm', and 's'
+  for (var a=0; a<lowered.length; a++) {
+    if (lowered[a] === 'h') { hCount++; }
+    if (lowered[a] === 'm') { mCount++; }
+    if (lowered[a] === 's') { sCount++; }
+  }
+
+  // Test for too many occurrences of the letters 'h', 'm', and 's'
+  if (hCount > 1 || mCount > 1 || sCount > 1) {
+    return null;
+  }
+
+  // Parse delay into pieces
+  pieces = /^([0-9]+[h|m|s])?([0-9]+[h|m|s])?([0-9]+[h|m|s])?$/.exec(lowered);
+  if (pieces === null) {
+    return null;
+  }
+
+  // Calculate milliseconds from pieces
+  for (var b=1; b<=3; b++) {
+    if (pieces[b]) {
+      if (pieces[b].slice(-1) === 'h') { milliseconds += pieces[b].slice(0, -1) * 1000 * 60 * 60; }
+      if (pieces[b].slice(-1) === 'm') { milliseconds += pieces[b].slice(0, -1) * 1000 * 60; }
+      if (pieces[b].slice(-1) === 's') { milliseconds += pieces[b].slice(0, -1) * 1000; }
+    }
+  }
+
+  return milliseconds;
+};
+
+exports.parseCustomStatusCode = function(status) {
+  if (_.isUndefined(status) || _.isNull(status) || _.isArray(status) || _.isObject(status)) {
+    return null;
+  }
+
+  return status;
+};
+
+exports.parseCustomHeaders = function(headers) {
+  if (!_.isObject(headers)) {
+    return null;
+  }
+
+  var err = _.find(_.values(headers), function(v) {
+    if (_.isUndefined(v) || _.isNull(v) || _.isArray(v) || _.isObject(v)) {
+      return true;
+    }
+  })
+
+  if (err) {
+    return null;
+  }
+
+  return headers;
 };

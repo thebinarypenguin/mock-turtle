0054c77353048bba576a88e56ed801478c4738a5
Ethan Zimmerman <thebinarypenguin@gmail.com>
2014-09-20 19:17:01 -0400

    Add tests for parsing functions

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,8 @@
 
 var should  = require('should'),
     request = require('supertest'),
-    server  = require('../src/server.js');
+    server  = require('../src/server.js'),
+    utils   = require('../src/utils.js');
 
 describe('Root Route', function() {
 
@@ -198,3 +199,89 @@ describe('Invalid Body', function() {
   });
 });
 
+describe('Parsing Functions', function() {
+
+  describe('Parse Delay', function() {
+    it('Should accept valid input', function(done) {
+      var valid = [
+        { delay: '1h2m3s', milliseconds: 3723000   },
+        { delay: '4h5m',   milliseconds: 14700000  },
+        { delay: '6m7s',   milliseconds: 367000    },
+        { delay: '8h9s',   milliseconds: 28809000  },
+        { delay: '90s',    milliseconds: 90000     },
+        { delay: '90m',    milliseconds: 5400000   },
+        { delay: '90h',    milliseconds: 324000000 }
+      ];
+
+      for (var i=0; i<valid.length; i++) {
+        utils.parseDelay(valid[i]['delay']).should.eql(valid[i]['milliseconds']);
+      }
+
+      done();
+    });
+
+    it('Should reject invalid input', function(done) {
+      var invalid = ['hh', 'mm', 'ss', '42', '1 m', '2s2m', {}, [], undefined, null, NaN, true, false];
+
+      for (var i=0; i<invalid.length; i++) {
+        (utils.parseDelay(invalid[i]) === null).should.be.true;
+      }
+
+      done();
+    });
+  });
+
+  describe('Parse Status Code', function() {
+    it('Should accept valid input', function(done) {
+      var valid = [100, "200", " 300 ", 4444, 5];
+
+      for (var i=0; i<valid.length; i++) {
+        (utils.parseCustomStatusCode(valid[i]) === null).should.be.false;
+      }
+
+      done();
+    });
+
+    it('Should reject invalid input', function(done) {
+      var invalid = ["foo", "2xx", "", {}, [], undefined, null, NaN, true, false];
+
+      for (var i=0; i<invalid.length; i++) {
+        (utils.parseCustomStatusCode(invalid[i]) === null).should.be.true;
+      }
+
+      done();
+    });
+  });
+
+  describe('Parse Headers', function() {
+    it('Should accept valid input', function(done) {
+      var valid = [
+        {
+          "foo": "bar"
+        },
+        {
+          "X-RateLimit-Limit" : 60,
+          "X-RateLimit-Remaining" : 56
+        }
+      ];
+
+      for (var i=0; i<valid.length; i++) {
+        (utils.parseCustomHeaders(valid[i]) === null).should.be.false;
+      }
+
+      done();
+    });
+
+    it('Should reject invalid input', function(done) {
+      var invalid = [
+        "{ key: value }", {}, [], undefined, null, NaN, true, false
+      ];
+
+      for (var i=0; i<invalid.length; i++) {
+        (utils.parseCustomHeaders(invalid[i]) === null).should.be.true;
+      }
+
+      done();
+    });
+  });
+});
